6f20684c166067ffb67c6b2a33e4d523aabe5719
mjedwards <jairedwards20@gmail.com>
2020-05-15 19:58:01 -0400

    initial commit

--- controllers/seller/createStore.js
+++ controllers/seller/createStore.js
@@ -1,39 +1,44 @@
-const Store = require('../../models/store');
-const { validateCreateStoreInput } = require('../../middleware/validateCreateStoreData');
+const Store = require("../../models/store");
+const {
+  validateCreateStoreInput,
+} = require("../../middleware/validateCreateStoreData");
 
 async function createStore(req, res) {
-	const { businessName } = req.body;
-	const { sub: sellerId } = req.decodedToken;
+  console.log(req.body);
+  const { businessName } = req.body;
+  const { sub: sellerId } = req.decodedToken;
 
-	const { errors, isValid } = validateCreateStoreInput(req.body);
-	if (!isValid) {
-		return res.status(400).json(errors);
-	}
+  const { errors, isValid } = validateCreateStoreInput(req.body);
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
 
-	try {
-		const result = await Store.findOne({ seller: sellerId });
+  try {
+    const result = await Store.findOne({ seller: sellerId });
 
-		if (result) {
-			return res.status(400).json({ message: 'You can not create more than one store' });
-			// } else {
-			//   const User = await Store.findOne({ storeName })
-			//   if (User) {
-			//     return res.status(400).json({ message: 'Store name already exists' })
-			//   }
-		}
-		const newStore = new Store({
-			businessName: req.body.businessName,
-			ownerName: req.body.ownerName,
-			address: req.body.address,
-			seller: req.decodedToken.sub
-		});
-		const saved = await newStore.save();
-		return res.status(201).json({
-			saved
-		});
-	} catch (err) {
-		return res.status(500).json({ message: err.message });
-	}
+    if (result) {
+      return res
+        .status(400)
+        .json({ message: "You can not create more than one store" });
+      // } else {
+      //   const User = await Store.findOne({ storeName })
+      //   if (User) {
+      //     return res.status(400).json({ message: 'Store name already exists' })
+      //   }
+    }
+    const newStore = new Store({
+      businessName: req.body.businessName,
+      ownerName: req.body.ownerName,
+      address: req.body.address,
+      seller: req.decodedToken.sub,
+    });
+    const saved = await newStore.save();
+    return res.status(201).json({
+      saved,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 }
 
 module.exports = createStore;
