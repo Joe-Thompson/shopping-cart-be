397f46ec3c4a2c80664a9c5ac92c4f8acb5ba595
mjedwards <jairedwards20@gmail.com>
2020-05-27 19:47:59 -0400

    initial commit

--- controllers/seller/createStore.js
+++ controllers/seller/createStore.js
@@ -4,41 +4,44 @@ const {
 } = require("../../middleware/validateCreateStoreData");
 
 async function createStore(req, res) {
-  console.log(req.body);
-  const { businessName } = req.body;
-  const { sub: sellerId } = req.decodedToken;
+
+	const { businessName, ownerName, address, secondAddress, city, state, zipCode, hours } = req.body;
+	const { sub: sellerId } = req.decodedToken;
 
   const { errors, isValid } = validateCreateStoreInput(req.body);
   if (!isValid) {
     return res.status(400).json(errors);
   }
 
-  try {
-    const result = await Store.findOne({ seller: sellerId });
 
-    if (result) {
-      return res
-        .status(400)
-        .json({ message: "You can not create more than one store" });
-      // } else {
-      //   const User = await Store.findOne({ storeName })
-      //   if (User) {
-      //     return res.status(400).json({ message: 'Store name already exists' })
-      //   }
-    }
-    const newStore = new Store({
-      businessName: req.body.businessName,
-      ownerName: req.body.ownerName,
-      address: req.body.address,
-      seller: req.decodedToken.sub,
-    });
-    const saved = await newStore.save();
-    return res.status(201).json({
-      saved,
-    });
-  } catch (err) {
-    return res.status(500).json({ message: err.message });
-  }
+		if (result) {
+			return res.status(400).json({ message: 'You can not create more than one store' });
+		}
+		else {
+			const User = await Store.findOne({ businessName });
+			if (User) {
+				return res.status(400).json({ message: 'Store name already exists' });
+			}
+		}
+		const newStore = new Store({
+			businessName: req.body.businessName,
+			ownerName: req.body.ownerName,
+			address: req.body.address,
+			secondAddress: req.body.secondAddress,
+			city: req.body.city,
+			state: req.body.city,
+			zipCode: req.body.zipCode,
+			hours: req.body.hours,
+			seller: req.decodedToken.sub
+		});
+		const saved = await newStore.save();
+		return res.status(201).json({
+			saved
+		});
+	} catch (err) {
+		return res.status(500).json({ message: err.message });
+	}
+
 }
 
 module.exports = createStore;
--- server.js
+++ server.js
@@ -1,24 +1,26 @@
-require("dotenv").config();
-const express = require("express");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const cors = require("cors");
-const path = require("path");
-const mongoose = require("mongoose");
-const mongoURI = require("./config/config");
-const passport = require("passport");
-const stripeAuth = require("./authentication/stripeAuthentication");
-const authRouter = require("./routes/authRouter");
-const productRouter = require("./routes/productRouter");
-const storeRouter = require("./routes/storeRouter");
-const cartRouter = require("./routes/cartRouter");
-const orderRouter = require("./routes/orderRouter");
-const paymentRouter = require("./routes/paymentRouter");
-const stripeAuthRouter = require("./routes/stripeAuthRouter");
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const mongoose = require('mongoose');
+const mongoURI = require('./config/config');
+const passport = require('passport');
+
+const stripeAuth = require('./authentication/stripeAuthentication');
+const authRouter = require('./routes/authRouter');
+const productRouter = require('./routes/productRouter');
+const storeRouter = require('./routes/storeRouter');
+const cartRouter = require('./routes/cartRouter');
+const paymentRouter = require('./routes/paymentRouter');
+const stripeAuthRouter = require('./routes/stripeAuthRouter');
+const orderRouter = require('./routes/orderRouter');
 
 const server = express();
-server.use(morgan("dev"));
+
 server.use(helmet());
+server.use(morgan('dev'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 
@@ -30,43 +32,48 @@ server.use(cors());
 server.use(passport.initialize());
 server.use(passport.session());
 
-server.use("/api/auth", authRouter);
-server.use("/api/store", productRouter);
-server.use("/api/store", storeRouter);
-server.use("/api/store", cartRouter);
-server.use("/api/store", orderRouter);
-server.use("/api/payment", paymentRouter);
-server.use("/api/auth/stripe", stripeAuthRouter);
+server.use('/api/auth', authRouter);
+server.use('/api/store', productRouter);
+server.use('/api/store', storeRouter);
+server.use('/api/store', cartRouter);
+server.use('/api/store', orderRouter);
+server.use('/api/payment', paymentRouter);
+server.use('/api/auth/stripe', stripeAuthRouter);
+
 
 passport.serializeUser((user, done) => {
-  done(null, user);
+	done(null, user);
 });
 passport.deserializeUser((user, done) => {
-  done(null, user);
+	done(null, user);
 });
 
 passport.use(stripeAuth);
 
-server.use(express.static(path.join(__dirname, "public")));
-server.set("views", path.join(__dirname, "views"));
-server.set("view engine", "pug");
+server.use(express.static(path.join(__dirname, 'public')));
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'pug');
 
 mongoose
-  .connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
+	.connect(
+		mongoURI,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useFindAndModify: false,
+			useCreateIndex: true
+		},
+		console.log('MongoDB connected')
+	)
+	.catch((err) => console.log(err));
 
-    useCreateIndex: true,
-  })
-  .catch((err) => console.log(err));
-
-server.get("/", (req, res) => {
-  res.status(200).send("Api is running!!");
+server.get('/', (req, res) => {
+	res.status(200).send('Api is running!!');
 });
 
-server.all("*", (req, res) => {
-  res.status(404).json({ message: "This URL can not be found" });
+server.all('*', (req, res) => {
+	res.status(404).json({ message: 'This URL can not be found' });
+
 });
 
 module.exports = server;
