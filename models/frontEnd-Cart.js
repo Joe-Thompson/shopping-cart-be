ee32adf1a6b822c3b8b0511ccdf6849b58bbf897
chiragthesia <harithesia@gmail.com>
2020-06-01 18:30:22 -0500

    start

--- models/frontEnd-Cart.js
+++ models/frontEnd-Cart.js
@@ -1,3 +1,9 @@
 import mongoose from 'mongoose';
 
-const feCartSchema = new mongoose.Schema({});
+const feCartSchema = new mongoose.Schema(
+	{
+		//product id - ref to product -
+		//variant id - ref to product - Send all variants for product.
+		//proudct qunt - number
+	}
+);
